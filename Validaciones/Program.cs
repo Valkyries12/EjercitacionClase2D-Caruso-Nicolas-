5b2906e208628b9624f1719539b609450353a817
ncaruso <nico.caruso1992@outlook.com>
2022-08-24 14:50:29 -0300

    ejercicio clase1_07

---
+++ Validaciones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Validaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
