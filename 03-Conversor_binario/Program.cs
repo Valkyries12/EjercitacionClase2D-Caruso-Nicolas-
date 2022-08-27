7a484b5e1dadbe9f3e4bad080e435a81ebf9cbb1
ncaruso <nico.caruso1992@outlook.com>
2022-08-27 20:28:03 -0300

    03-Conversor binario

---
+++ 03-Conversor_binario/Program.cs
@@ -0,0 +1,78 @@
+﻿using biblioteca_de_clases;
+using System;
+
+namespace _03_Conversor_binario
+{
+
+    /*
+     Desarollar una clase llamada Conversor que posea dos métodos de clase (estáticos):
+
+    El método ConvertirDecimalABinario que convierte un número entero del sistema decimal al sistema binario.
+
+    public string ConvertirDecimalABinario(int numeroEntero) {}
+
+    El método ConvertirBinarioADecimal que convierte un número entero del sistema binario al sistema decimal.
+
+    public int ConvertirBinarioADecimal(int numeroEntero) {}
+
+
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            string buffer;
+            bool noHayError;
+            string numeroConvertidoABinario;
+            int numeroConvertidoADecimal;
+
+            Console.WriteLine("=== INICIO DEL PROGRAMA ===");
+            
+            do
+            {
+                Console.Write("Ingrese el número a convertir a binario: ");
+                buffer = Console.ReadLine();
+                noHayError = int.TryParse(buffer, out numeroIngresado);
+
+                if (noHayError)
+                {
+                    numeroConvertidoABinario = Conversor.ConvertirDecimalAlBinario(numeroIngresado);
+                    Console.WriteLine($"{numeroIngresado} es : {numeroConvertidoABinario}");
+                } else
+                {
+                    Console.WriteLine("Error. No es un número mayor a 0. Reitente.");
+                }
+            } while (numeroIngresado < 1 && !noHayError);
+
+
+            do
+            {
+                Console.Write("Ingrese el número binario a convertir a decimal: ");
+                buffer = Console.ReadLine();
+
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    if (int.Parse(buffer[i].ToString()) > 1)
+                    {
+                        noHayError = false;
+                        break;
+                    }
+                }
+                //noHayError = int.TryParse(buffer, out numeroIngresado);
+
+                if (noHayError)
+                {
+                    numeroIngresado = int.Parse(buffer);
+                    numeroConvertidoADecimal = Conversor.ConvertirBinarioADecimal(numeroIngresado);
+                    Console.WriteLine($"{numeroIngresado} en decima es: {numeroConvertidoADecimal}");
+                } else
+                {
+                    Console.WriteLine("Error. Solo numeros binarios. Reintente.");
+                }
+            } while (!noHayError);
+
+            Console.WriteLine("=== FIN DEL PROGRAMA ===");
+        }
+    }
+}
---
+++ biblioteca_de_clases/Conversor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_de_clases
+{
+    public class Conversor
+    {
+        /// <summary>
+        /// Convierte unn número entero a binario
+        /// </summary>
+        /// <param name="numeroEntero">Numero entero</param>
+        /// <returns>retorna un numero binario en formato string</returns>
+        public static string ConvertirDecimalAlBinario(int numeroEntero)
+        {
+            string resultadoTemporal;
+            string resultadoFinal;
+            int cociente;
+            int dividendo;
+            int resto;
+            int divisionesHechas;
+
+            divisionesHechas = 0;
+            cociente = 0;
+            dividendo = numeroEntero;
+            resultadoTemporal = "";
+            resultadoFinal = "";
+            do
+            {
+                cociente = dividendo / 2;
+                resto = dividendo % 2;
+                resultadoTemporal += resto;
+                dividendo = dividendo / 2;
+                divisionesHechas++;
+                if (dividendo <= 1)
+                {
+                    resultadoTemporal += dividendo;
+                }
+            } while (dividendo != 1);
+
+
+            for (int i = divisionesHechas ; i >= 0; i--)
+            {
+                resultadoFinal += resultadoTemporal[i]; 
+            }
+
+
+            return resultadoFinal;
+        }
+
+
+        /// <summary>
+        /// Convierte un numero binario a decimal
+        /// </summary>
+        /// <param name="numeroEntero">numeroBinario en formato entero</param>
+        /// <returns>numero convertido a decimal</returns>
+        public static int ConvertirBinarioADecimal(int numeroEntero)
+        {
+            string binarioString;
+            double suma;
+            int numeroDecimal;
+            double exponente;
+
+            binarioString = numeroEntero.ToString();
+            exponente = binarioString.Length -1;
+            suma = 0;
+            for (int i = 0; i < binarioString.Length; i++)
+            {
+                if (binarioString[i] != '0')
+                {
+                    suma += Math.Pow(2, exponente);
+                }
+                exponente--;
+            }
+
+            numeroDecimal = (int) suma;
+            return numeroDecimal;
+        }
+    }
+}
