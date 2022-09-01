e28362d3037e5bd58e9b9d1ff4677b7cf67959dd
ncaruso <nico.caruso1992@outlook.com>
2022-08-31 22:58:05 -0300

    ejercicio ejemplo universal

---
+++ biblioteca_de_clases/Estudiante.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_de_clases
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public int NotaPrimerParcial
+        {
+            set { notaPrimerParcial = value; }
+        }
+
+        public int NotaSegundoParcial
+        {
+            set { notaSegundoParcial = value; }
+        }
+
+        private int CalcularPromedio()
+        {
+            return (notaPrimerParcial + notaSegundoParcial) / 2;
+        }
+
+        private int CalcularNotaFinal()
+        {
+            int notaFinal;
+
+            notaFinal = -1;
+            if (notaPrimerParcial > 3 && notaSegundoParcial > 3)
+            {
+                notaFinal = random.Next(6, 10);
+            }
+
+            return notaFinal;
+        }
+
+        public string Mostrar()
+        {
+            int notaFinal;
+
+            notaFinal = CalcularNotaFinal();
+            StringBuilder sbDatosDelAlumno = new StringBuilder($"Nombre: {nombre}");
+            sbDatosDelAlumno.AppendLine($"Apellido: {apellido}");
+            sbDatosDelAlumno.AppendLine($"Legajo: {legajo}");
+            sbDatosDelAlumno.AppendLine($"Primer parcial: {notaPrimerParcial}   Segundo Parcial: {notaSegundoParcial}");
+            sbDatosDelAlumno.AppendLine($"Promedio: {CalcularPromedio()}");
+
+            if (notaFinal > -1)
+            {
+                sbDatosDelAlumno.AppendLine($"Nota final: {notaFinal}");
+            }
+            else
+            {
+                sbDatosDelAlumno.AppendLine("Alumno desaprobado");
+            }
+
+                return sbDatosDelAlumno.ToString();
+        }
+    }
+}
---
+++ programacion_orientada_a_objetos/03-ejemplo_universal/Program.cs
@@ -0,0 +1,31 @@
+﻿using biblioteca_de_clases;
+using System;
+
+namespace _03_ejemplo_universal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== INICIO DEL PROGRAMA ===");
+
+            Estudiante pepe = new Estudiante("Argento", "1234", "Pepe");
+            Estudiante monica = new Estudiante("Argento", "321", "Monica");
+            Estudiante paola = new Estudiante("Argento", "456", "Paola");
+
+            pepe.NotaPrimerParcial = 7;
+            pepe.NotaSegundoParcial = 6;
+            monica.NotaPrimerParcial = 5;
+            monica.NotaSegundoParcial = 4;
+            paola.NotaPrimerParcial = 2;
+            paola.NotaSegundoParcial = 1;
+
+            Console.WriteLine(pepe.Mostrar());
+            Console.WriteLine(monica.Mostrar());
+            Console.WriteLine(paola.Mostrar());
+
+            Console.WriteLine("=== FIN DEL PROGRAMA ===");
+
+        }
+    }
+}
